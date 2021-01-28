c78a7b11c95b21334123ec7cab5391ca7c41c824
Javier Cancela <javiercancela@users.noreply.github.com>
2021-01-28 11:09:40 +0100

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,8 @@ cd ../site
 git clone -b master https://github.com/javiercancela/javiercancela.github.io.git
 cd javiercancela.github.io/
 rm -rf ./*
+cp -R ../../javiercancela.github.io/_site/* ./
+git add -A .
+git commit -m "Deploy"
+git push origin master
+
